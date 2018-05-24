00dba87059bab3baa15d269695ebafc50d840c9d
yazeenj <yazeenj@outlook.com>
2018-05-24 22:27:56 +0200

    Dheap working further checks needed

--- dHeap.h
+++ dHeap.h
@@ -2,7 +2,6 @@
 #define DHEAP_H
 
 #include "IHeap.h"
-#include <iostream>
 
 template <typename T>
 class dHeap : public IHeap<T>
@@ -13,10 +12,23 @@ private:
 	T *elements;
 	int nrOfelements;
 
-public:
-	dHeap(int d);
-	~dHeap();
+	//Heap functions
+	int getParent(int index);
+	int getChild(int index, int childPlace);
+	int findBiggestChild(int index);
 
+public:
+	
+	dHeap(int d);
+	//constructer with size for HeapAscendingSort
+	dHeap(int d, int size);
+	~dHeap();
+	
+	//consrtuct&overide
+	dHeap(const dHeap &other);
+	void operator=(const dHeap &other);
+
+	//Extend heapsize
 	void extend();
 
 	//imp-functions
@@ -26,8 +38,40 @@ public:
 	int size() const;
 	int getdValue() const;
 
+	//Extras
+	bool isEmpty() const;
 };
 
+template <typename T>
+dHeap<T>::dHeap(const dHeap &other) {
+	this->dValue = other.dValue;
+	this->heapSize = other.heapSize;
+	this->nrOfelements = other.nrOfelements;
+
+	this->elements = new T[this->heapSize];
+
+	for (int i = 0; i < this->nrOfelements; i++) {
+		this->elements[i] = other.elements[i];
+	}
+
+}
+
+template <typename T>
+void dHeap<T>::operator=(const dHeap &other) {
+	if (this != &other) {
+		this->dValue = other.dValue;
+		this->heapSize = other.heapSize;
+		this->nrOfelements = other.nrOfelements;
+
+		delete[] this->elements;
+		this->elements = new T[this->heapSize];
+
+		for (int i = 0; i < this->nrOfelements; i++) {
+			this->elements[i] = other.elements[i];
+		}
+
+	}
+}
 
 template <typename T>
 void dHeap<T>::extend() {
@@ -42,9 +86,34 @@ void dHeap<T>::extend() {
 
 template <typename T>
 dHeap<T>::dHeap(int d) {
+	
+	//d(d >1).
+	if (d > 1) {
+		this->dValue = d;
+	}
+	else {
+		this->dValue = 2;
+	}
 
-	this->dValue = d;
+	//init private-variables
 	this->heapSize = 5;
+	this->nrOfelements = 0;
+	this->elements = new T[heapSize];
+}
+
+template <typename T>
+dHeap<T>::dHeap(int d, int size) {
+
+	//d(d >1).
+	if (d > 1) {
+		this->dValue = d;
+	}
+	else {
+		this->dValue = 2;
+	}
+
+	//init private-variables
+	this->heapSize = size;
 	this->nrOfelements = 0;
 	this->elements = new T[heapSize];
 }
@@ -56,39 +125,75 @@ dHeap<T>::~dHeap() {
 
 template <typename T>
 void dHeap<T>::push(const T & item) {
+	//If heap is full = extend
 	if (this->nrOfelements == this->heapSize) {
-
 		this->extend();
+	}
 
-		this->elements[this->nrOfelements] = item;
-		this->nrOfelements++;
+	//Get index of the last parent
+	int parentIndex = this->getParent(this->nrOfelements);
+
+	//if this is not the first parent
+	if (parentIndex != -1) {
+
+		int newItemIndex = this->nrOfelements;
+		//place at the end
+		this->elements[newItemIndex] = item;
+
+		//Swap until heap priority
+		while (parentIndex != -1 && item > this->elements[parentIndex]) {
+
+			this->elements[newItemIndex] = this->elements[parentIndex];
+			this->elements[parentIndex] = item;
+
+			newItemIndex = parentIndex;
+			parentIndex = this->getParent(newItemIndex);
+
+		}
 
 	}
+	//in case of first inserted item
 	else {
-		this->elements[this->nrOfelements] = item;
-		this->nrOfelements++;
+		this->elements[0] = item;
 	}
 
+	this->nrOfelements++;
 }
 
 template <typename T>
 T dHeap<T>::pop() {
-	if (this->nrOfelements == 0) {
+	
+	//If heap is empty
+	if (this->isEmpty()) {
 		return -1;
 	}
-	T temp = this->elements[0];
+
+	T returnItem = this->elements[0];
 	this->elements[0] = this->elements[this->nrOfelements - 1];
 	this->nrOfelements--;
-	return temp;
+
+	int parentIndex = 0;
+	int biggestChild = this->findBiggestChild(parentIndex);
+	T tempChild;
+
+	while (biggestChild != -1 && this->elements[biggestChild] > this->elements[parentIndex]) {
+		tempChild = this->elements[biggestChild];
+		this->elements[biggestChild] = this->elements[parentIndex];
+		this->elements[parentIndex] = tempChild;
+		parentIndex = biggestChild;
+		biggestChild = this->findBiggestChild(parentIndex);
+	}
+
+	return returnItem;
+
 }
 
 template <typename T>
 T dHeap<T>::peek() const {
 
-	if (this->nrOfelements == 0) {
+	if (this->isEmpty()) {
 		return -1;
 	}
-
 	return this->elements[0];
 }
 
@@ -102,5 +207,80 @@ int dHeap<T>::getdValue() const {
 	return this->dValue;
 }
 
+//Private functions
+
+template <typename T>
+int dHeap<T>::getParent(int index) {
+	/*
+	Parent = ((index-1)/d)
+	*/
+
+	int returnVal = 0;
+
+	//In-case of index is parent/root
+	if (index == 0) {
+		returnVal = -1;
+	}
+	else {
+
+		returnVal = (index - 1) / this->dValue;
+
+	}
+
+	return returnVal;
+}
+
+template <typename T>
+int dHeap<T>::getChild(int index, int childPlace) {
+	/*
+		Left/right child
+		Child = index*2+1
+	*/
+
+	int returnVal = 0;
+	
+	if (childPlace < 1 || childPlace > this->dValue) {
+		childPlace = 1;
+	}
+
+	returnVal = index * this->dValue + childPlace;
+	
+	//In case of non-complete parent
+	if (returnVal >= this->nrOfelements) {
+		returnVal = -1;
+	}
+	return returnVal;
+}
+
+template <typename T>
+int dHeap<T>::findBiggestChild(int index) {
+	
+	int biggestChild = this->getChild(index,1);
+	int nextChild = 0;
+
+	if (biggestChild != -1) {
+		//Min > 2
+		for (int i = 2; i <= this->dValue && nextChild != -1; i++) {
+			nextChild = this->getChild(index, i);
+			if (nextChild != -1) {
+				//this->elements[biggestChild] > this->elements[nextChild]
+				if (this->elements[biggestChild] < this->elements[nextChild]) {
+					biggestChild = nextChild;
+				}
+			}
+		}
+	}
+
+	return biggestChild;
+}
+
+//Extras
+template <typename T>
+bool dHeap<T>::isEmpty() const {
+	if (this->nrOfelements == 0) {
+		return true;
+	}
+	return false;
+}
 
 #endif 
